fdbe0c72ed551c9a590bd6deb807b40f96e8e138
jayce-hash <jayce@concertocity.com>
2026-07-18 09:43:26 -0500

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,16 @@
+/* Concerto analytics loader.
+   To activate: create a GA4 property at analytics.google.com,
+   then replace the placeholder ID below with your G- ID. One file, whole site. */
+(function () {
+  var ID = 'G-XXXXXXXXXX';
+  if (ID.indexOf('XXXXXXXX') !== -1) return; // not configured yet, do nothing
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + ID;
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', ID);
+})();
